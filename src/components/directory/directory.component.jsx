81c87f4aa13fe106f09170345100ad06dc8c8665
Popstar <henry.erabor@orbsolutions.com.ng>
2020-04-06 09:58:08 +0100

    added routing and navigation to the app

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -4,7 +4,7 @@ import './directory.styles.scss'
 
 
 class Directory extends React.Component {
-  constructor() {
+  constructor(props) {
     super()
     this.state = {
       sections: [
@@ -47,8 +47,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({title,imageUrl, id, size}) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+        {this.state.sections.map(({id, ...otherSectionProps}) => (
+          <MenuItem key={id} {...otherSectionProps}/>
         ))}
       </div>
     )
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
+import { withRouter } from "react-router-dom"
 import './menu-item.styles.scss'
 
-const MenuItem = ({ title,imageUrl,size }) => (
-  <div style={{
-    backgroundImage:`url(${imageUrl})`
-  }} className={`${size} menu-item`}>
+const MenuItem = ({ title,imageUrl,size, history, linkUrl, match }) => (
+  <div onClick={ () => history.push(`${match.url}${linkUrl}`)} style={{backgroundImage:`url(${imageUrl})`}} className={`${size} menu-item`}>
     <div className='background-image' style={{
       backgroundImage:`url(${imageUrl})`
     }}/>
@@ -15,4 +14,4 @@ const MenuItem = ({ title,imageUrl,size }) => (
     </div>
   </div>
 )
-export default MenuItem
+export default withRouter(MenuItem)
