9150c52784b8e0b7bdeb473f5ab1913abe297873
Popstar <henry.erabor@orbsolutions.com.ng>
2020-04-28 02:39:57 +0100

    Moved directory to reducer

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,27 +1,22 @@
-import React from "react"
-import SHOP_DATA from "./shop.data"
-import CollectionPreview from "../../components/preview-collection/collection-preview.component"
+import React from 'react'
+import { Route } from 'react-router-dom'
+import CollectionPage from '../collection/collection.component'
 
-class Shop extends React.Component {
-  constructor(){
-    super()
-    this.state = {
-      collections:SHOP_DATA
-    }
-  }
-  render(){
-    const {collections} = this.state
-    return (
-      <div className="shop-page">
-        {
-          collections.map(({id, ...otherCollection}) => (
-            <CollectionPreview key={id} {...otherCollection}></CollectionPreview>
-          ))
-        }
-      </div>
-    )
-  }
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component'
+const ShopPage = ({ match }) => {
+  return (
+    <div className="shop-page">
+      <Route
+        exact
+        path={`${match.path}`}
+        component={CollectionsOverview}
+      ></Route>
+      <Route
+        path={`${match.path}/:collectionId`}
+        component={CollectionPage}
+      ></Route>
+    </div>
+  )
 }
 
-
-export default Shop
+export default ShopPage
---
+++ src/redux/shop/shop.selectors.js
@@ -0,0 +1,18 @@
+import { createSelector } from "reselect"
+export const selectShop = state => state.shop
+
+export const selectCollections = createSelector(
+  selectShop,
+  shop => shop.collections
+)
+
+
+export const selectCollection = (collectionURLParam) => createSelector(
+  selectCollections,
+  collections => collections[collectionURLParam]
+)
+
+export const selectCollectionsforPreview = createSelector(
+  selectCollections,
+  collections => Object.keys(collections).map(key => collections[key])
+)
